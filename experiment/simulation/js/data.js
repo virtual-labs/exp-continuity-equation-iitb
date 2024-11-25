1a36c1d20f03b135f1a261755008a596374641f6
pushpdeep <>
2024-11-25 06:46:10 +0000

    added payload

---
+++ experiment/simulation/js/data.js
@@ -0,0 +1,39 @@
+// ======== Activity 1 variables ====== //
+let d1_a1 = 12;
+let d2_a1 = 16;
+let v1_a1 = 5;
+let v2_a1;
+let A1_a1;
+let A2_a1;
+let Q_a1;
+// ===== Activity 2 variables ===== //
+let d1_a2 = 30;
+let d2_a2 = 20;
+let d3_a2 = 15;
+let A1_a2;
+let A2_a2;
+let A3_a2;
+let Q1_a2;
+let Q2_a2;
+let Q3_a2;
+let v1_a2 = 3;
+let v2_a2 = 2;
+let v3_a2;
+// ===== Activity 3 variables ===== //
+let d1_a3 = 1.2; //ab
+let d2_a3 = 1.5; //bc
+let d3_a3 = 0.8; //cd
+let d4_a3 = 0.5; //ce
+let A_ab;
+let A_bc;
+let A_cd;
+let A_ce;
+let Q_ab;
+// let Q_bc: number;
+let Q_cd;
+let Q_ce;
+let v_ab = 3;
+let v_bc;
+let v_cd = 2;
+let v_ce;
+//# sourceMappingURL=data.js.map
